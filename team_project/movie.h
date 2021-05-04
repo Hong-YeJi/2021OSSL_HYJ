7c9c4645e5c34f66a6015f7bc90a522c3e72860d
yeji <1q2w3e7884@naver.com>
2021-05-04 11:59:49 +0900

    create movie.h

---
+++ team_project/movie.h
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+
+typedef struct {
+  char movie_name[100];
+  char director[50];
+  float star;
+  char genre[30];
+  char date[15];
+  int ranking;
+} Movie;
+
+//Menu
+int selectMenu();
+int selectDataNo(Movie s[], int count);
+
+//CRUD
+void Create(Movie *s, int count);
+void Read(Movie *s, int count);
+void Update(Movie *s, int num);
+void Delete(Movie *s, int num);
+
+//Save&Load
+void saveData(Movie *s, int count);
+int loadData(Movie *s);
+
+//Search
+void searchMovie(Movie *s, int count);
+void searchStar(Movie *s, int count);
+void searchDirector(Movie *s, int count);
+void searchGenre(Movie *s, int count);
